30fa3ce37b482862bac7c9a90263a60e4670de18
Loreto Notarantonio <loreto.n@gmail.com>
2026-06-21 17:32:09 +0200

    added type annotations to context.py e colors.py (Release 0.1.8)

--- src/pyLnLib/colors.py
+++ src/pyLnLib/colors.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 #
 # updated by ...: Loreto Notarantonio
-# Date .........: 21-06-2026 17.19.40
+# Date .........: 21-06-2026 17.30.02
 #
+from dataclasses import dataclass
 
 # ##################################################
 # # colori
 # ##################################################
+@dataclass(frozen=True)  # "frozen" rende i colori non modificabili per errore
 class Color:
+    """Classe per i codici colore ANSI per il terminale."""
     red: str = "\033[31m"
     redH: str = "\033[91m"
     green: str = "\033[32m"
@@ -16,6 +19,8 @@ class Color:
     yellowH: str = "\033[93m"
     blue: str = "\033[34m"
     blueH: str = "\033[94m"
+    purple: str = "\033[35m"
+    purpleH: str = "\033[95m"
     magenta: str = "\033[35m"
     magentaH: str = "\033[95m"
     cyan: str = "\033[36m"
@@ -24,6 +29,18 @@ class Color:
     whiteH: str = "\033[97m"
     reset: str = "\033[0m"
 
-    purple: str = magenta
-    purpleH: str = magentaH
-# C=Color
+    # Costanti per stili aggiuntivi
+    bold: str = "\033[1m"
+    underline: str = "\033[4m"
+    blink: str = "\033[5m"
+    reverse: str = "\033[7m"
+    hidden: str = "\033[8m"
+
+    # Background colors
+    bg_red: str = "\033[41m"
+    bg_green: str = "\033[42m"
+    bg_yellow: str = "\033[43m"
+    bg_blue: str = "\033[44m"
+    bg_magenta: str = "\033[45m"
+    bg_cyan: str = "\033[46m"
+    bg_white: str = "\033[47m"
