49e6710a2450fc1d1c838226c5f1afa15320b10f
Loreto Notarantonio <loreto.n@gmail.com>
2026-06-21 17:20:19 +0200

    added type annotations to colors.py (Release 0.1.8)

--- src/pyLnLib/colors.py
+++ src/pyLnLib/colors.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
 #
 # updated by ...: Loreto Notarantonio
-# Date .........: 10-05-2026 15.18.43
+# Date .........: 21-06-2026 17.19.40
 #
 
 # ##################################################
 # # colori
 # ##################################################
 class Color:
-    red = "\033[31m"
-    redH = "\033[91m"
-    green = "\033[32m"
-    greenH = "\033[92m"
-    yellow = "\033[33m"
-    yellowH = "\033[93m"
-    blue = "\033[34m"
-    blueH = "\033[94m"
-    # purple     = '\033[35m'; purpleH    = '\033[95m'
-    magenta = "\033[35m"
-    magentaH = "\033[95m"
-    cyan = "\033[36m"
-    cyanH = "\033[96m"
-    white = "\033[37m"
-    whiteH = "\033[97m"
-    reset = "\033[0m"
+    red: str = "\033[31m"
+    redH: str = "\033[91m"
+    green: str = "\033[32m"
+    greenH: str = "\033[92m"
+    yellow: str = "\033[33m"
+    yellowH: str = "\033[93m"
+    blue: str = "\033[34m"
+    blueH: str = "\033[94m"
+    magenta: str = "\033[35m"
+    magentaH: str = "\033[95m"
+    cyan: str = "\033[36m"
+    cyanH: str = "\033[96m"
+    white: str = "\033[37m"
+    whiteH: str = "\033[97m"
+    reset: str = "\033[0m"
 
-    purple = magenta
-    purpleH = magentaH
-
-
+    purple: str = magenta
+    purpleH: str = magentaH
 # C=Color
